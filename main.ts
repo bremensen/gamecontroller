5295e75f847bcddaf75d30b4139a55d6156d2f61
bremensen <matthieucollege13@gmail.com>
2024-11-25 12:22:57 +0100

    è

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+basic.forever(function () {
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Up) || Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Down)) {
+        basic.showString("1")
+        basic.pause(2000)
+        basic.clearScreen()
+    } else {
+        if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Up) && Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Down)) {
+            basic.showString("1")
+            basic.pause(2000)
+            basic.clearScreen()
+        }
+    }
+})
