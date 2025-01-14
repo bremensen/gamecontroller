44bfa71a0970ff85ba012773030c31f14c512368
bremensen <matthieucollege13@gmail.com>
2025-01-14 15:06:44 +0100

    ML,µ,o

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
 basic.forever(function () {
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Up) || Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Down)) {
-        basic.showString("1")
-        basic.pause(2000)
-        basic.clearScreen()
-    } else {
-        if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Up) && Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Down)) {
-            basic.showString("1")
-            basic.pause(2000)
-            basic.clearScreen()
+    basic.showNumber(0)
+})
+basic.forever(function () {
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Up) && Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right)) {
+        basic.showNumber(1)
+    } else if (false) {
+        if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Down) && Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left)) {
+            basic.showNumber(1)
         }
     }
 })
