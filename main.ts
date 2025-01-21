705738c390004cb151bb5ad05c8eb75041baf5c4
bremensen <matthieucollege13@gmail.com>
2025-01-21 13:15:15 +0100

    bjgkeqg

--- main.ts
+++ main.ts
@@ -1,39 +1,32 @@
 input.onButtonPressed(Button.A, function () {
-    voeding += 1
+    voeding += 5
 })
 input.onButtonPressed(Button.B, function () {
-    knuffel += 1
+    knuffel += 5
 })
-let dood = 0
-let knuffel = 0
 let voeding = 20
+let knuffel = 20
+let dood = 1
 basic.forever(function () {
-    if ((knuffel && voeding) >= 10) {
+	
+})
+basic.forever(function () {
+    if (knuffel > 10 && voeding > 10) {
         basic.showIcon(IconNames.Happy)
     }
-    if (voeding < 10) {
-        basic.showIcon(IconNames.Sad)
-    }
-    if (voeding && 0 == 0) {
+    if (knuffel <= 0 && voeding <= 0) {
         basic.showIcon(IconNames.Asleep)
     }
-})
-basic.forever(function () {
-    if (knuffel == 1) {
+    if (knuffel <= 10 && voeding <= 10 && (voeding < 0 && knuffel > 0)) {
         basic.showIcon(IconNames.Sad)
     }
-    if (knuffel == dood) {
-        basic.showIcon(IconNames.Asleep)
+    if ((knuffel && voeding) <= 10 && (knuffel && voeding) > 0) {
+        basic.showIcon(IconNames.Sad)
     }
 })
 basic.forever(function () {
     voeding += -1
     basic.pause(1000)
-})
-basic.forever(function () {
-    dood = 0
-})
-basic.forever(function () {
     knuffel += -1
     basic.pause(1000)
 })
